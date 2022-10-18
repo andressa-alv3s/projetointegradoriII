b0912121d705600e59187b74dc0f55252f0aa80f
Andressa Alves <dessa.alv3s@gmail.com>
2022-10-18 19:47:52 -0300

    ...

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -17,11 +17,15 @@ public class HomeController : Controller
     {
         return View();
     }
-
-    public IActionResult Privacy()
+    public IActionResult Login()
     {
         return View();
     }
+    public IActionResult Cursos()
+    {
+        return View();
+    }
+
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
